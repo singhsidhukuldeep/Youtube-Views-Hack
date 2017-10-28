ee7db8f64ac668b81f62c9ce719f316b3e9e0deb
Kuldeep Singh Sidhu <singhsidhukuldeep@gmail.com>
2017-10-29 03:27:12 +0530

    php integeration

--- index.php
+++ index.php
@@ -160,18 +160,36 @@ Repository: https://github.com/singhsidhukuldeep/Youtube-Views-Hack
                                 <div class="col-lg-10">
                                     <select class="form-control" id="Inst" name="Inst">
                                         <option>0 (Select according  to your available bandwidth)</option>
-                                        <option>1</option>
+                                        <!-- <option>1</option> -->
                                         <option>2</option>
-                                        <option>3</option>
+                                        <!-- <option>3</option> -->
                                         <option>4</option>
-                                        <option>5</option>
+                                        <!-- <option>5</option> -->
                                         <option>6</option>
-                                        <option>7</option>
+                                        <!-- <option>7</option> -->
                                         <option>8</option>
-                                        <option>9</option>
+                                        <!-- <option>9</option> -->
                                         <option>10</option>
-                                        <option>11</option>
+                                        <!-- <option>11</option> -->
                                         <option>12</option>
+                                        <!-- <option>13</option> -->
+                                        <option>14</option>
+                                        <!-- <option>15</option> -->
+                                        <option>16</option>
+                                        <!-- <option>17</option> -->
+                                        <option>18</option>
+                                        <!-- <option>19</option> -->
+                                        <option>20</option>
+                                        <!-- <option>21</option> -->
+                                        <option>22</option>
+                                        <!-- <option>23</option> -->
+                                        <option>24</option>
+                                        <!-- <option>25</option> -->
+                                        <option>26</option>
+                                        <!-- <option>27</option> -->
+                                        <option>28</option>
+                                        <!-- <option>29</option> -->
+                                        <option>30</option>
                                     </select>
                                 </div>
                             </div>
